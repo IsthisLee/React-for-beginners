44bd4513ec8a9e422ae3339a84f46632f83386fa
IsthisLee <rjsgmldnwn@gmail.com>
2022-07-03 21:04:18 +0900

    prac(effects): #6.2 Deps

--- react-cra/src/App.js
+++ react-cra/src/App.js
@@ -2,11 +2,29 @@ import { useEffect, useState } from 'react';
 
 function App() {
   const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState('');
   const onClick = () => setCounter((prev) => prev + 1);
-  console.log('i run all the time');
-  useEffect(() => console.log('CALL THE API...'), []);
+  const onChange = (event) => setKeyword(event.target.value);
+  useEffect(() => {
+    console.log('I run only once.');
+  }, []);
+  useEffect(() => {
+    console.log("I run when 'keyword' changes.");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  }, [counter]);
+  useEffect(() => {
+    console.log("I run when 'keyword' || 'counter' changes.");
+  }, [keyword, counter]);
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
       <h1>{counter}</h1>
       <button onClick={onClick}>Click me</button>
     </div>
