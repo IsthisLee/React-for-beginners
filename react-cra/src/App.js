0524a7f25c113824e54afcc7e0d0a75ca9bebd38
IsthisLee <rjsgmldnwn@gmail.com>
2022-07-03 20:45:37 +0900

    prac(effects): #6.1 useEffect

--- react-cra/src/App.js
+++ react-cra/src/App.js
@@ -1,9 +1,10 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   const [counter, setCounter] = useState(0);
   const onClick = () => setCounter((prev) => prev + 1);
-  console.log('render');
+  console.log('i run all the time');
+  useEffect(() => console.log('CALL THE API...'), []);
   return (
     <div>
       <h1>{counter}</h1>
