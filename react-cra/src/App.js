053aac55c2ce4390d7ff823a063378b0c0cf5a24
IsthisLee <rjsgmldnwn@gmail.com>
2022-07-04 17:43:12 +0900

    prac: #7.0~7.1 To Do List

--- react-cra/src/App.js
+++ react-cra/src/App.js
@@ -1,20 +1,29 @@
-import { useEffect, useState } from 'react';
-
-function Hello() {
-  useEffect(() => {
-    console.log('hi :)');
-    return () => console.log('bye :(');
-  }, []);
-  return <h1>Hello</h1>;
-}
+import { useState } from 'react';
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setToDo] = useState('');
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === '') return;
+    setToDo('');
+    setToDos((currentArr) => [...currentArr, toDo]);
+  };
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? 'Hide' : 'Show'}</button>
+      <h1>My Todos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          value={toDo}
+          onChange={onChange}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr/>
+      {toDos.map((item, idx) => <li key={idx}>{item}</li> )}
     </div>
   );
 }
