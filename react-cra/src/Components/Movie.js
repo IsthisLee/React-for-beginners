21f6ed40cda0fdd367c43239f944ac4e9799f7c6
IsthisLee <rjsgmldnwn@gmail.com>
2022-07-17 21:30:32 +0900

    prac(React): #7.6 Parameters

--- react-cra/src/Components/Movie.js
+++ react-cra/src/Components/Movie.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-function Movie({ coverImg, title, summary, genres, id }) {
+function Movie({ id, coverImg, title, summary, genres }) {
   return (
     <div>
       <img src={coverImg} alt={title} />
@@ -19,6 +19,7 @@ function Movie({ coverImg, title, summary, genres, id }) {
 }
 
 Movie.propTypes = {
+  id: PropTypes.number.isRequired,
   coverImg: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
--- react-cra/src/Pages/Home.js
+++ react-cra/src/Pages/Home.js
@@ -24,11 +24,11 @@ function Home() {
           {movies.map((movie) => (
             <Movie
               key={movie.id}
+              id={movie.id}
               coverImg={movie.medium_cover_image}
               title={movie.title}
               summary={movie.summary}
               genres={movie.genres}
-              id={movie.id}
             />
           ))}
         </div>
