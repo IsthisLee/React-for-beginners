0f639153644ebbf3a2a1e9338ef7c57af0ff4567
IsthisLee <rjsgmldnwn@gmail.com>
2022-07-17 21:52:41 +0900

    prac(React): #7.9 Styles(본문 235자 제한) + Detail Page Loading처리

--- react-cra/src/Pages/Detail.js
+++ react-cra/src/Pages/Detail.js
@@ -1,19 +1,24 @@
 import { useParams } from 'react-router-dom';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 function Detail() {
   const { id } = useParams();
+  const [movie, setMovie] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
   const getMovie = async () => {
-    const data = await fetch(
+    const movie = await fetch(
       `https://yts.mx/api/v2/movie_details.json?movie_id=${id}`
     ).then((res) => res.json());
+    setMovie(movie);
+    setIsLoading(false);
   };
 
   useEffect(() => {
     getMovie();
   }, []);
 
-  return <h1>Detail</h1>;
+  return <div>{isLoading ? <h1>Loading...</h1> : <h1>Detail</h1>}</div>;
 }
 
 export default Detail;
