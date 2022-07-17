5f24f460348a4df04bdc8cb884d3b5bcf040c6c2
IsthisLee <rjsgmldnwn@gmail.com>
2022-07-17 20:22:29 +0900

    prac(React): #7.3 Movie App part One

--- react-cra/src/App.js
+++ react-cra/src/App.js
@@ -2,61 +2,33 @@ import { useEffect, useState } from 'react';
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [selectedCoin, setSelectedCoin] = useState('');
-  const [userMoney, setUserMoney] = useState(0);
-  const [transferredValue, setTransferredValue] = useState(0);
-
-  const onChangeCoin = (e) => {
-    setSelectedCoin(JSON.parse(e.target.value));
-    transferredValue &&
-      setTransferredValue(
-        userMoney / JSON.parse(e.target.value).quotes.USD.price
-      );
-  };
-  const userMoneyHandler = (e) => setUserMoney(e.target.value);
-  const onClickTransfer = (e) => {
-    e.preventDefault();
-    setTransferredValue(userMoney / selectedCoin.quotes.USD.price);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const response = await fetch(
+      `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+    ).then((res) => res.json());
+    setMovies(response.data.movies);
+    setIsLoading(false);
   };
   useEffect(() => {
-    fetch('https://api.coinpaprika.com/v1/tickers')
-      .then((res) => res.json())
-      .then((json) => {
-        setCoins(json);
-        setIsLoading(false);
-        setSelectedCoin(json[0]);
-      });
+    getMovies();
   }, []);
+
   return (
     <div>
-      <h1>The Coins! {isLoading ? '' : `(${coins.length})`}</h1>
       {isLoading ? (
-        <strong>Loading...</strong>
+        <h1>Loading...</h1>
       ) : (
         <div>
-          <h3>Transfer USD to Coin!</h3>
-          <select onChange={onChangeCoin}>
-            {coins.map((coin) => (
-              <option value={JSON.stringify(coin)} key={coin.id}>
-                {coin.name} ({coin.symbol}): ${coin.quotes.USD.price}
-              </option>
-            ))}
-          </select>
-          <form>
-            <input
-              type="number"
-              placeholder="input your USD"
-              onChange={userMoneyHandler}
-              value={userMoney}
-            />
-            <button onClick={onClickTransfer}>Let's transfer!</button>
-          </form>
-          {!!transferredValue && (
-            <span>
-              You can buy {transferredValue} {selectedCoin.name}
-            </span>
-          )}
+          {movies.map((movie) =>
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image} />
+              <h2>{movie.title}</h2>
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map((genre) => <li key={genre}>{genre}</li>)}
+              </ul>
+            </div>)}
         </div>
       )}
     </div>
