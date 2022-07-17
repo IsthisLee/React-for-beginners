23f37bc0597899b738f465a934c918193adcabd1
IsthisLee <rjsgmldnwn@gmail.com>
2022-07-17 21:00:36 +0900

    prac(React): #7.4 Movie App part Two

--- react-cra/src/App.js
+++ react-cra/src/App.js
@@ -1,38 +1,5 @@
-import { useEffect, useState } from 'react';
-
 function App() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    const response = await fetch(
-      `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
-    ).then((res) => res.json());
-    setMovies(response.data.movies);
-    setIsLoading(false);
-  };
-  useEffect(() => {
-    getMovies();
-  }, []);
-
-  return (
-    <div>
-      {isLoading ? (
-        <h1>Loading...</h1>
-      ) : (
-        <div>
-          {movies.map((movie) =>
-            <div key={movie.id}>
-              <img src={movie.medium_cover_image} />
-              <h2>{movie.title}</h2>
-              <p>{movie.summary}</p>
-              <ul>
-                {movie.genres.map((genre) => <li key={genre}>{genre}</li>)}
-              </ul>
-            </div>)}
-        </div>
-      )}
-    </div>
-  );
+  return null;
 }
 
 export default App;
---
+++ react-cra/src/Pages/Home.js
@@ -0,0 +1,39 @@
+import { useEffect, useState } from '@types/react';
+import Movie from '../Components/Movie';
+
+function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const response = await fetch(
+      `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+    ).then((res) => res.json());
+    setMovies(response.data.movies);
+    setIsLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  return (
+    <div>
+      {isLoading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <Movie
+              key={movie.id}
+              coverImg={movie.medium_cover_image}
+              title={movie.title}
+              summary={movie.summary}
+              genres={movie.genres}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Home;
