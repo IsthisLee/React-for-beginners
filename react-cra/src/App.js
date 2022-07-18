b6c2a6289aa5c77e022fee5710022a2a89f99faf
IsthisLee <rjsgmldnwn@gmail.com>
2022-07-18 19:48:35 +0900

    fix(React): Router에 basename추가 -> github pages 빈 화면 이슈 해결

--- react-cra/src/App.js
+++ react-cra/src/App.js
@@ -4,7 +4,7 @@ import Detail from './Pages/Detail';
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/movie/:id" component={Detail} />
